d27d85f971b06f334b7402dbf7d7f2e2de7880f2
Sandeep <sarswatsandeep1997@gmail.com>
2019-10-05 13:19:20 +0530

    message

--- client/dist/shareapp/styles-es2015.js
+++ client/dist/shareapp/styles-es2015.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/sandeep/Desktop/final/ShareApp/shareapp/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
-module.exports = __webpack_require__(/*! /home/sandeep/Desktop/final/ShareApp/shareapp/src/styles.css */"./src/styles.css");
+__webpack_require__(/*! /home/sandeep/Desktop/testfolder/ShareApp/client/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
+module.exports = __webpack_require__(/*! /home/sandeep/Desktop/testfolder/ShareApp/client/src/styles.css */"./src/styles.css");
 
 
 /***/ })
